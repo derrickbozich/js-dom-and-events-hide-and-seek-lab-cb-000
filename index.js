771195bb12d5317968acb7933afcfe2499331239
derrick b <dbozich1@gmail.com>
2018-10-09 22:11:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,10 +23,22 @@ function deepestChild(){
   return deepestChild;
 }
 
-function increaseRankBy(n){
-  let li = document.querySelectorAll('.ranked-list li');
-  for (let element of li){
-    element.innerText = parseInt(element.innerText) + n;
+// function increaseRankBy(n){
+//   let li = document.querySelectorAll('.ranked-list li');
+//   for (let element of li){
+//     element.innerText = parseInt(element.innerText) + n;
+//
+//   }
+// }
 
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
   }
 }
