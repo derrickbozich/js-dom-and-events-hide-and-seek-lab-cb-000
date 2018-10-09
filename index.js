f748c8f2b82f4fd41dda12efe7ce4ba7fcb31fb0
derrick b <dbozich1@gmail.com>
2018-10-09 21:28:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,18 +6,30 @@ function nestedTarget(){
   return document.getElementById("nested").querySelector('.target');
 }
 
-function deepestChild(){
-  const div = document.getElementById("grand-node");
-  let deepestChild;
-  function getChildren(div){
-    let element = div.children[0];
-    if (element != null) {
-      deepestChild = element;
-      return getChildren(element);
-    } else {
-      return deepestChild;
-    }
+// function deepestChild(){
+//   const div = document.getElementById("grand-node");
+//   let deepestChild;
+//   function getChildren(div){
+//     let element = div.children[0];
+//     if (element != null) {
+//       deepestChild = element;
+//       return getChildren(element);
+//     } else {
+//       return deepestChild;
+//     }
+// 
+//   }
+// 
+}
 
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
   }
 
+  return node
 }
