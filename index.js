2ff6b20c5b9a86bd6b3fcb0a1ed14acddb1d89f5
derrick b <dbozich1@gmail.com>
2018-10-09 21:12:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.getElementById("nested").querySelector('.target');
 }
+
+function deepestChild(){
+  const div = document.getElementById("grand-node");
+  let deepestChild;
+  for (let element of div.children[0]){
+    
+    if (element.children[0] != null){
+      debugger
+      deepestChild = element.children[0];
+    } else {
+      return deepestChild;
+    }
+  }
+  
+}
